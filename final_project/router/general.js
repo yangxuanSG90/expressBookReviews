2a08cbc12170791ac9394d567009507ae43748d1
yangxuanSG90 <yangxuan90@gmail.com>
2024-03-30 03:07:08 -0400

    task 11,12,13,14

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -41,29 +41,64 @@ public_users.get('/',function (req, res) {
 
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-        const isbn = req.params.isbn;
-        res.send(books[isbn])
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+// TASK 11 - Get book details based on ISBN using Promises
+public_users.get('/books/isbn/:isbn',function (req, res) {
+    const get_books_isbn = new Promise((resolve, reject) => {
+    const isbn = req.params.isbn;
+    // console.log(isbn);
+        if (req.params.isbn <= 10) {
+        resolve(res.send(books[isbn]));
+    }
+        else {
+            reject(res.send('ISBN not found'));
+        }
+    });
+    get_books_isbn.
+        then(function(){
+            console.log("Promise for Task 11 is resolved");
+   }).
+        catch(function () { 
+                console.log('ISBN not found');
+  });
+
+});
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+// TASK 12 - Get book details based on author
+public_users.get('/books/author/:author',function (req, res) {
+
+    const get_books_author = new Promise((resolve, reject) => {
+
     let booksbyauthor = [];
-  let isbns = Object.keys(books);
-  isbns.forEach((isbn) => {
-    if(books[isbn]["author"] === req.params.author) {
-      booksbyauthor.push({"isbn":isbn,
-                          "title":books[isbn]["title"],
-                          "reviews":books[isbn]["reviews"]});
-    }
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["author"] === req.params.author) {
+        booksbyauthor.push({"isbn":isbn,
+                            "title":books[isbn]["title"],
+                            "reviews":books[isbn]["reviews"]});
+      resolve(res.send(JSON.stringify({booksbyauthor}, null, 4)));
+      }
+
+
+    });
+    reject(res.send("The mentioned author does not exist "))
+        
+    });
+
+    get_books_author.then(function(){
+            console.log("Promise is resolved");
+   }).catch(function () { 
+                console.log('The mentioned author does not exist');
   });
-  res.send(JSON.stringify({booksbyauthor}, null, 4));
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+
+  });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
+
+    const get_books_title = new Promise((resolve, reject) => {
+        
     let booksbytitle = [];
     let isbns = Object.keys(books);
     isbns.forEach((isbn) => {
@@ -74,27 +109,34 @@ public_users.get('/title/:title',function (req, res) {
       }
     });
     res.send(JSON.stringify({booksbytitle}, null, 4));
-  return res.status(300).json({message: "Yet to be implemented"});
 });
+    reject(res.send("The mentioned title does not exist "))
+    
+});
+
+get_books_title.then(function(){
+        console.log("Promise is resolved");
+}).catch(function () { 
+            console.log('The mentioned title does not exist');
+});
+
+
 
 //  Get book review
 
-public_users.get('/review/:isbn',function (req, res){
+const axios = require('axios');
 
-    const isbnParam = req.params.isbn;
-
-    // Get the book reviews based on ISBN provided in the request parameters
-
-    const reviews = books[isbnParam];
-
-    if (!reviews) {
-      res.status(404).json({ message: 'No reviews found for the ISBN provided' });
-    } else {
-      res.status(200).json(reviews);
-
-  return res.status(300).json({message: "Yet to be implemented"});
-    }
-    
+public_users.get('/books', function (req, res) {
+    axios.get('http://your-api-url/books')
+        .then(response => {
+            res.send(JSON.stringify(response.data.books, null, 4));
+            console.log("Promise for Task 10 resolved");
+        })
+        .catch(error => {
+            // Handle error
+            console.error('Error fetching books:', error);
+            res.status(500).send('Error fetching books');
+        });
 });
 
 module.exports.general = public_users;
